2be0c26131e7ccabfa2a95cf953d68e30d3bb706
renuvijo <renuvijo@gmail.com>
2024-08-08 09:40:52 -0400

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,10 @@
+
+
+const sum = (num1, num2) => num1 + num2;
+const difference = (num1, num2) => num1 - num2;
+const product = (num1, num2) => num1 * num2;
+const division = (num1, num2) => num1 / num2;
+
+module.exports = {
+    sum, difference, product, division
+}
